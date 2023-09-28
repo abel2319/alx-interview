173c5e28bf0c2bf7f78cbf2d794a6d8a40836e82
abel2319 <abelbakary@gmail.com>
2023-09-28 09:45:06 +0100

    initialization

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+"""
+pascal triangle module
+"""
+
+def pascal_triangle(n):
+    """
+    Return the triangle
+    """
+    if n > 0:
+        if n == 1:
+            return [[1]]
+        elif n == 2:
+            return [[1], [1, 1]]
+        else:
+            row = [1,1]
+            tmp = []
+            pascal_triangle = [[1],[1,1]]
+            for i in range(2,n):
+                tmp.append(1)
+                j = 0
+                for j in range(1,i):
+                    tmp.append(row[j-1] + row[j])
+                tmp.append(1)
+                pascal_triangle.append(tmp)
+                row = tmp
+                tmp = []
+    return pascal_triangle
