7a98340493da24a631bc5f8f7c7a0d50fe8c06bf
abel2319 <abelbakary@gmail.com>
2023-10-10 04:59:58 +0100

    feat(task0): task 0 ✅

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Lockboxes
+"""
+
+def canUnlockAll(boxes):
+    """
+        args(list): box
+        return true or false
+    """
+    if boxes:
+        n_boxes = [0]
+        n = len(n_boxes)
+        for i in boxes:
+            for j in i:
+                if j < len(boxes) and j not in n_boxes and j != boxes.index(i):
+                    n_boxes.append(j)
+        if len(n_boxes) == len(boxes):
+            return True
+    return False
